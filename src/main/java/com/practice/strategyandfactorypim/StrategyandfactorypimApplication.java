9b2b1b6f175b76d32d410e196fab0b44a3fce626
Rishav Dev <rishavbhardwaj82@gmail.com>
2026-04-06 07:20:46 +0530

    Initail application setup

---
+++ src/main/java/com/practice/strategyandfactorypim/StrategyandfactorypimApplication.java
@@ -0,0 +1,13 @@
+package com.practice.strategyandfactorypim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyandfactorypimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyandfactorypimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/strategyandfactorypim/StrategyandfactorypimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.strategyandfactorypim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyandfactorypimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
